5dcec9360b91e5d5eb4458427a72116f10075e3f
Richard Kiss <him@richardkiss.com>
2015-02-10 17:13:57 -0500

    Make default port a function of network. yapf error: pycoinnet/helpers/networks.py invalid syntax (<unknown>, line 20) [gitreformat yapf-ify (github/ghtdak) on Sat Jul 16 09:30:36 2016] [from commit: f7dc57c3c042f449b1fd649e9a881d7ce9c9ddc4]

--- pycoinnet/helpers/networks.py
+++ pycoinnet/helpers/networks.py
@@ -2,17 +2,20 @@
 
 import binascii
 
-MAINNET = dict(MAGIC_HEADER=binascii.unhexlify('F9BEB4D9'),
-               DNS_BOOTSTRAP=[
-                   "seed.bitcoin.sipa.be",
-                   "dnsseed.bitcoin.dashjr.org"
-                   "bitseed.xf2.org",
-                   "dnsseed.bluematt.me",
-               ])
+MAINNET = dict(
+    MAGIC_HEADER = binascii.unhexlify('F9BEB4D9'),
+    DNS_BOOTSTRAP = [
+        "seed.bitcoin.sipa.be", "dnsseed.bitcoin.dashjr.org"
+        "bitseed.xf2.org", "dnsseed.bluematt.me",
+    ],
+    DEFAULT_PORT = 8333,
+)
 
-TESTNET = dict(MAGIC_HEADER=binascii.unhexlify('0B110907'),
-               DNS_BOOTSTRAP=[
-                   "bitcoin.petertodd.org",
-                   "testnet-seed.bitcoin.petertodd.org", "bluematt.me",
-                   "testnet-seed.bluematt.me"
-               ])
+TESTNET = dict(
+    MAGIC_HEADER = binascii.unhexlify('0B110907'),
+    DNS_BOOTSTRAP = [
+        "bitcoin.petertodd.org", "testnet-seed.bitcoin.petertodd.org",
+        "bluematt.me", "testnet-seed.bluematt.me"
+    ]
+    DEFAULT_PORT = 18333,
+)
