0b33090f7b7ef114070e35a4538804524b34b193
Richard Kiss <him@richardkiss.com>
2016-04-13 11:30:22 -0700

    Some improvements to dnsbootstrap.py to make it testable.
    [gitreformat yapf-ify (github/ghtdak) on Sat Jul 16 09:30:40 2016]
    [from commit: 154805395458e2cd9ea84febabb8537fcb0676e6]

--- pycoinnet/helpers/dnsbootstrap.py
+++ pycoinnet/helpers/dnsbootstrap.py
@@ -14,7 +14,12 @@ from pycoinnet.helpers.standards import version_data_for_peer
 from pycoinnet.peer.BitcoinPeerProtocol import BitcoinPeerProtocol
 
 
-def dns_bootstrap_host_port_q(network_info):
+def dns_bootstrap_host_port_q(network_info, getaddrinfo):
+    """
+    Accepts network_info type (from pycoinnet.helpers.networks) and returns an asyncio.queue.
+
+    You MUST call queue.task.close() on the return value when you're done with it.
+    """
     dns_bootstrap = network_info["DNS_BOOTSTRAP"]
 
     superpeer_ip_queue = asyncio.Queue()
@@ -23,8 +28,7 @@ def dns_bootstrap_host_port_q(network_info):
     def bootstrap_superpeer_addresses(dns_bootstrap):
         for h in dns_bootstrap:
             try:
-                r = yield from asyncio.get_event_loop().getaddrinfo(
-                    h, network_info["DEFAULT_PORT"])
+                r = yield from getaddrinfo(h, network_info["DEFAULT_PORT"])
                 results = set(t[-1][:2] for t in r)
                 for t in results:
                     yield from superpeer_ip_queue.put(t)
@@ -39,7 +43,10 @@ def dns_bootstrap_host_port_q(network_info):
 
 
 def new_queue_of_timestamp_peeraddress_tuples(
-        network_info, timestamp_peeraddress_tuple_queue=None):
+        network_info,
+        timestamp_peeraddress_tuple_queue=None,
+        create_connection=asyncio.get_event_loop().create_connection,
+        getaddrinfo=asyncio.get_event_loop().getaddrinfo):
     """
     Returns a queue which is populated with (time, host, port) tuples of
     addresses of regular peers that we can connect to.
@@ -50,7 +57,8 @@ def new_queue_of_timestamp_peeraddress_tuples(
 
     magic_header = network_info["MAGIC_HEADER"]
 
-    superpeer_ip_queue = dns_bootstrap_host_port_q(network_info)
+    superpeer_ip_queue = dns_bootstrap_host_port_q(network_info,
+                                                   getaddrinfo=getaddrinfo)
 
     if timestamp_peeraddress_tuple_queue is None:
         timestamp_peeraddress_tuple_queue = PriorityQueue()
@@ -65,10 +73,10 @@ def new_queue_of_timestamp_peeraddress_tuples(
                 peer_name = "%s:%d" % pair
                 host, port = pair
                 logging.debug("connecting to superpeer at %s", peer_name)
-                transport, peer = yield from asyncio.get_event_loop(
-                ).create_connection(lambda: BitcoinPeerProtocol(magic_header),
-                                    host=host,
-                                    port=port)
+                transport, peer = yield from create_connection(
+                    lambda: BitcoinPeerProtocol(magic_header),
+                    host=host,
+                    port=port)
 
                 logging.debug("connected to superpeer at %s", peer_name)
                 yield from initial_handshake(peer, version_data_for_peer(peer))
@@ -78,7 +86,7 @@ def new_queue_of_timestamp_peeraddress_tuples(
 
                 logging.debug("got addresses from %s", peer_name)
                 for da in date_address_tuples:
-                    timestamp_peeraddress_tuple_queue.put_nowait(
+                    yield from timestamp_peeraddress_tuple_queue.put(
                         (-da[0], da[1]))
                 logging.debug("closing connection to %s", peer_name)
                 transport.close()
@@ -92,22 +100,30 @@ def new_queue_of_timestamp_peeraddress_tuples(
 
     return timestamp_peeraddress_tuple_queue
 
-
-@asyncio.coroutine
-def show(timestamp_address_queue):
-    while 1:
-        timestamp, addr = yield from timestamp_address_queue.get()
-        logging.info("@ %s with address %s", timestamp, addr)
+    return timestamp_peeraddress_tuple_queue
 
 
 def main():
+
+    @asyncio.coroutine
+    def show(timestamp_address_queue):
+        for r in range(100):
+            pair = yield from timestamp_address_queue.get()
+            if pair is None:
+                break
+            timestamp, addr = pair
+            logging.info("@ %s with address %s", timestamp, addr)
+
+    from pycoinnet.helpers.networks import MAINNET
+    network = MAINNET
     asyncio.tasks._DEBUG = True
     logging.basicConfig(level=logging.DEBUG,
                         format=('%(asctime)s [%(process)d] [%(levelname)s] '
                                 '%(filename)s:%(lineno)d %(message)s'))
-    timestamp_address_queue = new_queue_of_timestamp_peeraddress_tuples()
-    asyncio.Task(show(timestamp_address_queue))
-    asyncio.get_event_loop().run_forever()
+    timestamp_address_queue = new_queue_of_timestamp_peeraddress_tuples(network)
+    t = asyncio.Task(show(timestamp_address_queue))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(t)
 
 
 if __name__ == '__main__':
